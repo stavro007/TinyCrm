a54afef0c7861ba6d32e96330a03a37a242523aa
Spyros <Spyros@DESKTOP-EHCEVFL>
2020-04-28 18:26:25 +0300

    initial1 commit

--- TinyCrm.console/Program.cs
+++ TinyCrm.console/Program.cs
@@ -7,6 +7,7 @@ namespace TinyCrm.console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
